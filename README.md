91ede4df2ee25280801bb995c57482d75478a147
husterdjx <76643896@qq.com>
2023-05-28 16:42:43 +0800

    finish in win

---
+++ README.md
@@ -0,0 +1,38 @@
+# auto-paste
+A simple python script to automatically abstract->paste->empty the contents of your clipboard.
+
+## quick start
+1. Clone the repo
+```bash
+git clone https://github.com/husterdjx/auto-paste.git
+```
+### python environment
+2. Install the requirements
+```bash
+pip3 install -r requirements.txt
+# or pip install -r requirements.txt
+```
+3. Run the script
+```bash
+python3 paste.py
+# or python paste.py
+```
+### conda environment
+2. Create a conda environment
+```bash
+conda create -n auto-paste python=3.8
+```
+3. Activate the environment
+```bash
+conda activate auto-paste
+```
+4. Install the requirements
+```bash
+pip install -r requirements.txt
+```
+5. Run the script
+```bash
+python paste.py
+```
+
+Enjoy!
--- paste.py
+++ paste.py
@@ -4,26 +4,43 @@ import pyperclip
 from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
-def getClipboardData():
-    p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
-    retcode = p.wait()
-    data = p.stdout.read()
-    #这里的data为bytes类型，之后需要转成utf-8操作
-    return data
+import platform
+print("Current OS playform: {}".format(platform.system().lower()))
+
+if platform.system().lower() == 'windows':
+    def getClipboardData():
+        data = pyperclip.paste()
+        return data
+elif platform.system().lower() == 'linux':
+    def getClipboardData():
+        p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
+        retcode = p.wait()
+        data = p.stdout.read()
+        txt=str(data,'utf-8')
+        txt=txt.strip().replace('\r\n',' ').replace('\r',' ').replace('\n',' ')
+        return txt
+else:
+    def getClipboardData():
+        p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
+        retcode = p.wait()
+        data = p.stdout.read()
+        txt=str(data,'utf-8')
+        txt=txt.strip().replace('\r\n',' ').replace('\r',' ').replace('\n',' ')
+        return txt
 
 while True:
     pyperclip.copy("")
     while pyperclip.paste() == '':
         time.sleep(0.1)
     #获取剪切板内容
-    txt=str(getClipboardData(),'utf-8')
-    txt=txt.strip().replace('\r\n',' ').replace('\r',' ').replace('\n',' ')
+    txt=getClipboardData()
+    print("--------get clipboard data--------")
     print(txt)
+    print("--------end end end end end--------")
     time.sleep(3)
-    #重新转成bytes型
     list = [one for one in txt]
     # 写入剪切板
     for one in list:
-        print(one)
+        # print(one)
         keyboard.type(one)
         time.sleep(0.05)
